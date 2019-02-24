25dfaaa637da42ecc01be50f3ccd88c4ec1a4e68
Noah A <nanderson815@gmail.com>
2019-02-24 15:36:32 -0500

    addede seeds db

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+
+var mysql = require("mysql");
+
+var connection = mqsql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "admin",
+    database: "burgers_db"
+});
+
+connection.connect(function(err){
+    if (err){
+        console.log("Error: ", err);
+    } else {
+        console.log("Connected as ID ", connection.threadId);
+    };
+});
+
+module.exports = connection;
