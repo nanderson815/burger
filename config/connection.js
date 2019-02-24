495721cc36a0b72ee1bb9580692da9d8b0cad689
Noah A <nanderson815@gmail.com>
2019-02-24 16:53:20 -0500

    added handlebars layout

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 
 var mysql = require("mysql");
 
-var connection = mqsql.createConnection({
+var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
--- server.js
+++ server.js
@@ -3,3 +3,22 @@ var app = express();
 
 var PORT = process.env.PORT || 4000;
 
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
