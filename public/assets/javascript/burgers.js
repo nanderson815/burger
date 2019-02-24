21be75ae2dd58631ac514d60259bc8513d70b732
Noah A <nanderson815@gmail.com>
2019-02-24 17:19:42 -0500

    added eating the burger

--- public/assets/javascript/burgers.js
+++ public/assets/javascript/burgers.js
@@ -1,7 +1,25 @@
 $(function () {
 
-    $("button").click(function(){
-        console.log("click click");
+    $(".change-status").click(function () {
+        var id = $(this).data("id")
+        var newDevoured = $(this).data("newdevoured");
+        console.log("ID: ", id);
+        console.log("NewD: ", newDevoured);
+
+        var newDevouredState = {
+            devoured: newDevoured
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function () {
+                console.log("Ate the burger");
+                location.reload();
+            }
+        );
+
     });
 
 
