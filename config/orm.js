c01b420a6e62a2f2843f72d5c6c8a015e93a5f53
Noah A <nanderson815@gmail.com>
2019-02-24 15:54:13 -0500

    adding orm

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,68 @@
 var connection = require("../config/connection.js");
 
 
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
 var orm = {
-    selectAll = function(table, cb){
+    selectAll: function (table, cb) {
         var queryString = "SELECT * FROM ";
         queryString += table;
-        connection.query(queryString, function(err, data){
+        connection.query(queryString, function (err, data) {
             if (err) throw err;
             cb(data);
         });
     },
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO ";
+        queryString += table;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES ";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, data) {
+            if (err) {
+                throw err;
+            }
+            cb(data);
+        });
+    },
+    updateOne: function (table, objColVals, condition, cb){
+        var queryString = "Update " + table;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, data){
+            if (err) throw err;
+            cb(data);
+        });
+    }
 }
 
 
