31d88396321cc99c972b9a3ccf5a888945f5ce35
Noah A <nanderson815@gmail.com>
2019-02-24 16:32:31 -0500

    setting up handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,8 +4,8 @@ var burger = require("../models/burger.js");
 
 var router = express.Router();
 
-router.get("/", function(req, res){
-    burger.selectAll(function(data){
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
         var hbsObject = {
             burgers: data
         };
@@ -14,9 +14,23 @@ router.get("/", function(req, res){
     });
 });
 
-router.post("/api/burgers", function(req, res){
-    burger.insertOne("burger_name", req.body.burger_name, function(data){
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne("burger_name", req.body.burger_name, function (data) {
         res.json({ id: data.insertId });
     });
 });
 
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function (data) {
+        if (data.affectedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
