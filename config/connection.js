50c348af4fa18073a9171079e0f0504a75c03e61
Noah A <nanderson815@gmail.com>
2019-02-28 19:58:25 -0500

    adding cloud connection settings

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,32 @@
 
 var mysql = require("mysql");
+require('dotenv').config();
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "admin",
-    database: "burgers_db"
-});
+var config;
+
+if (!process.env.INSTANCE_CONNECTION_NAME){
+    config = {
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "admin",
+        database: "burgers_db"
+    };
+} else{
+
+    config = {
+        socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+        user: process.env.SQL_USER,
+        password: process.env.SQL_PASSWORD,
+        database: process.env.SQL_DATABASE
+    };
+
+}
+
+
+
+
+connection = mysql.createConnection(config);
 
 connection.connect(function(err){
     if (err){
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-require('dotenv').config();
 var app = express();
 
 var PORT = process.env.PORT || 4000;
